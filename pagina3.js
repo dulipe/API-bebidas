4a7b1fd9d0e5eee31981945e0417e4feaba97492
dulipe <filipe_opc2@hotmail.com>
2021-06-08 10:30:25 -0300

    finalizado

--- pagina3.js
+++ pagina3.js
@@ -1,33 +1,6 @@
-const search = async(param) =>{
+const search = async() =>{
     document.querySelector('.resultado').innerHTML = ''
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${param}`).then(res => res.json())
-    for (let i = 0; i < response.drinks.length; i++) {
-        // criação das strings e foto
-        let drink = response.drinks
-        let id = document.createElement('p')
-        let nome = document.createElement('p')
-        let foto = document.createElement('img')
-
-        // apender informações nos elementos criados
-        id.append(drink[i].idDrink)
-        nome.append(drink[i].strDrink)
-        foto.setAttribute('src', drink[i].strDrinkThumb)
-
-        // apender informações dentro de uma div
-        const div = document.createElement('div')
-        div.setAttribute('class', 'bebidaInfo')
-        div.append(id, nome, foto)
-        div.setAttribute('id', drink[i].idDrink)
-        div.setAttribute('onclick', 'bebidaInfo(this.id)')
-        
-        // apender no html
-        document.querySelector('.resultado').append(div)
-    }
-}
-
-const bebidaInfo = async(param) =>{
-    document.querySelector('.resultado').innerHTML = ''
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${param}`).then(res => res.json())
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php').then(res => res.json())
     for (let i = 0; i < response.drinks.length; i++) {
         // criação das strings e foto
         let drink = response.drinks
